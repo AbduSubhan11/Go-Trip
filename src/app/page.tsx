1abae78c60816c53163f96ecdfbf634d3fbfb316
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2024-10-27 23:35:29 +0500

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ import PlateformOverview from "./components/Plateform Overview";
 export default function Home() {
   return (
     <section>
-      <div className="max-w-full h-screen bg-[url('/Images/heroes.png')] bg-cover bg-center flex flex-col items-center justify-center"></div>
+      <div className="max-w-full h-screen bg-[url('/Images/hero.jpg')] bg-cover bg-center md:py-60 py-52 md:px-32 px-8 space-y-8">
+        <h1 className="text-[#FDE02F] md:text-7xl text-5xl ">
+          𝓕𝓲𝓷𝓭 𝔂𝓸𝓾𝓻 𝓝𝓮𝔁𝓽 𝓽𝓸𝓾𝓻!
+        </h1>
+        <h1 className="text-white text-2xl">Where would you like to go?</h1>
+      </div>
       <TourDetails />
       <FavouritePlaces />
       <PlateformOverview />
