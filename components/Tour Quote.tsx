47a9a25a79174e5194ddf2c244d334b603ab035d
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2024-11-06 10:31:47 +0500

    complete

--- components/Tour Quote.tsx
+++ components/Tour Quote.tsx
@@ -1,15 +1,18 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 function TourQuote() {
-    return (
-        <div className="bg-[url('/Images/buildings.jpg')] bg-cover bg-center bg-opacity-10 bg-fixed text-white mb-20 space-y-8 p-6 h-[400px] md:h-[500px] flex flex-col justify-center items-center">
-            <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold md:px-20 lg:px-48 text-center">
-                Some places are so good to stay in, you do not want to generate a rush on them.
-            </h1>
-            <p className="text-base md:text-lg lg:text-xl font-bold underline">Mpho N - Trip Advisor</p>
-        </div>
-    );
+  return (
+    <div className="bg-[url('/Images/buildings.jpg')] bg-cover bg-center bg-opacity-10 bg-fixed text-white mb-20 space-y-8 p-6 h-[400px] md:h-[500px] flex flex-col justify-center items-center">
+      <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold md:px-20 lg:px-48 text-center">
+        Some places are so good to stay in, you do not want to generate a rush
+        on them.
+      </h1>
+      <p className="text-base md:text-lg lg:text-xl font-bold underline">
+        Mpho N - Trip Advisor
+      </p>
+    </div>
+  );
 }
 
 export default TourQuote;
